cde18276b51e36fe91edc2d62cc255ac9e36ee57
Andrew Phillips <phillips4energy@gmail.com>
2014-11-30 10:53:51 -0600

    Update best.R
    Cleaned up comments and shortened some code

--- best.R
+++ best.R
@@ -28,7 +28,7 @@ best <- function(state, outcome) {
         #If found return a list TRUE or FALSE for the outcome that matched
         outcome.found <- lapply(conditions,function(elt) elt[1:length(elt)] == outcome)
         
-        #Convert to logical vector in order to test for logical values
+        #Convert to logical vector in order to test for logical values since lapply returns a list
         outcome.found <- as.logical(outcome.found)
         
         #Check for TRUE value in the logical list and if any value is TRUE than we found our outcome
@@ -40,82 +40,88 @@ best <- function(state, outcome) {
                 }  
         }
       
-        #if(outcome.found[1:length(conditions) == TRUE]) {
-        
         #Validate outcome argument
-        if(vald == TRUE) {
-              # print(outcome)
-        }else{
-                stop("invalid outcome")
-        }
+        if(!vald) stop("invalid outcome")
+            
         
-        #Get best mortality for heart attack
+        ###############################################
+        ##Get best mortality for Heart Attack Outcome
+        ###############################################k
         if(outcome == "heart attack"){          
           
-          #Get Hospital Name, State and Heart Attack data from data frame
+                #Get Hospital Name(column 2), State(column 7) and Heart Failure(column 11) column data from data frame
           heartAttackDf <- hosp.data[1:hosp.rows,c(2,7,11)] 
           
-          #Get all row index that match the state 
+          #Subset the dataframe and get all vertical rows from the data frame for a particular index that match the stat 
           heartAttackByState <- heartAttackDf[which(heartAttackDf[,"State"] == state),]
           
-          #Subset the heart attack dataframe and remove NA's from Heart Attack column(i.e. col 11 all rows)
+          #Subset the heart attack dataframe object for all rows and remove NA's from Heart Failure column(i.e. col 3)
           heartAttackNoNaDf <- subset(heartAttackByState,  heartAttackByState[,3] != "NA") 
           
-          #Convert the column to numeric as it was a character
-          #heartAttackNoNaDf <- transform(heartAttackNoNaDf, 
-                                         #Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack 
-                                         #= as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))
-          
+          #Order the heart attack data by ratings
           orderit <- heartAttackNoNaDf[order(heartAttackNoNaDf$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),]
        
         }
         
+        ###############################################
+        ##Get best mortality for Heart Faliure Outcome
+        ###############################################
         if(outcome == "heart failure"){
-                #Get Hospital Name, State and Heart Failure data from data frame
+                #Get Hospital Name(column 2), State(column 7) and Heart Failure(column 17) column data from data frame
                 heartFailDf <- hosp.data[1:hosp.rows,c(2,7,17)] 
                 
-                #Get all row index that match the state 
+                #Subset the dataframe and get all vertical rows from the data frame for a particular index that match the state 
                 heartFailByState <- heartFailDf[which(heartFailDf[,"State"] == state),]
                 
-                #Subset the heart attack dataframe and remove NA's from Heart Attack column(i.e. col 11 all rows)
+                #Subset the heart failure dataframe object for all rows and remove NA's from Heart Failure column(i.e. col 3)
                 heartFailNoNaDf <- subset(heartFailByState,  heartFailByState[,3] != "NA") 
-                #heartFailNoNaDf <- transform(heartFailNoNaDf, Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
                 
+                #Order the heart failure data by ratings
                 orderit <- heartFailNoNaDf[order(heartFailNoNaDf$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),]               
         }
         
+        ###############################################
+        ##Get best mortality for Pneumonia Outcome
+        ###############################################
         if(outcome == "pneumonia"){
-                #Get Hospital Name, State and Pneumonia column data from data frame
+                #Get Hospital Name(column 2), State(column 7) and Pneumonia(column 23) column data from data frame
                 pneumoniaDf <- hosp.data[1:hosp.rows,c(2,7,23)] 
                 
-                #test <- which(pneumoniaDf[,"State"] == state)
                 #Subset the dataframe and get all vertical rows from the data frame for a particular index that match the state 
                 pneumoniaByState <- pneumoniaDf[which(pneumoniaDf[,"State"] == state),]
                 
-                #Subset the heart attack dataframe and remove NA's from Heart Attack column(i.e. col 11 all rows)
+                #Subset the pneumonia dataframe object for all rows and remove NA's from Pneumonia column(i.e. col 3)
                 pneumoniaNoNaDf <- subset(pneumoniaByState,  pneumoniaByState[,3] != "NA") 
-                #pneumoniaNoNaDf <- transform(pneumoniaNoNaDf, Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia = as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
                 
+                #Order the pnemonia data by ratings
                 orderit <- pneumoniaNoNaDf[order(pneumoniaNoNaDf$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),]       
         
         }
         
-        #Return the first element and column. Need to add code for a tie.
-        
+        #Return the first element and column.     
         return(orderit[1,1])
+        
         #return(head(orderit,4)) for testing output
 }
 
 
 
-
+####################################################
+# checkState
+# Description: check that the state passed in is in the dataset
+# Arguments: dataframe object, state
+#
+####################################################
 checkState <- function(data, state, isValid = TRUE){
               
               #Get State column data or could have split it using st <- split(hosp.data$State,hosp.data$State) and 
               #get the names(st)
               allState1 <- data$State 
+              
+              #Subset the state vectore and search it to see if state is valid i.e. found
               st <- allState1[allState1 == state]
 
+              #If st vector is empty than the state is invalid and stop the process, else return TRUE result
               if(length(st) == 0){
                       stop("invalid state")
               }else{
